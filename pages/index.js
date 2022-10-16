aa78e17d8b59f45f6278a9c8d10a39f5ba8cd1f4
Scott Tran <scotttran@utexas.edu>
2022-10-15 21:12:32 -0500

    redirecting when logged in

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar'
+
 export default function Home() {
   return (
     <>
