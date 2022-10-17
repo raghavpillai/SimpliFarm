d0f128c60d5a6d117b1d122673f2b1b274e93c8c
Raghav <raghavpillai101@gmail.com>
2022-10-17 15:41:16 -0500

    Removed print

--- server/src/modules/generation.py
+++ server/src/modules/generation.py
@@ -5,10 +5,7 @@ import os
 from dotenv import load_dotenv
 
 env_path=os.path.join('../', '.env.local')
-
 load_dotenv(env_path)
-
-print(os.getenv("COHERE_KEY"))
 
 co_client = cohere.Client(os.getenv("COHERE_KEY"))
 
@@ -25,7 +22,6 @@ def send_msg(msg, image, number):
     )
 
 def generate_response(name, number, total_price, wp_sf, sp_sf, ps, pw, pf, image):
-    print(number)
     response = co_client.generate(
         model='large', 
         prompt='This is a text generator that takes sample data and tells the user the data. \n\nSample: Raghav, total price is 391.9, used water per square foot is 93, used fertilizer per square foot is 91, predicted stress is 30, predicted water is 12, predicted fertilizer is 40\nOutput Text: Hey Raghav, SimpliFarm here. Thanks for using our service. Your estimated 14 day total was $391.9, with water per square foot taking $54, and fertilizer per square foot taking $45. Our analysis shows that predicted plant stress is 30%, predicted water usage is $12, and predicted fertilizer usage is $40. \n--\nSample: Rishi, total price is 31.4, used water per square foot is 48, used fertilizer per square foot is 19, predicted stress is 19, predicted water is 32, predicted fertilizer is 14\nOutput Text: Good afternoon Rishi, thanks for using SimpliFarm! We ran the numbers and came down to the estimated 14 day total being $31.4, with water per square foot at $48, and fertilizer per square foot at $19. Our analysis indicates that plant stress is at 19%, with predicted water at $32, and predicted fertilizer is $14. \n--\nSample: Nam, total price is 123.1, used water per square foot is 93, used fertilizer per square foot is 91, predicted stress is 38, predicted water is 39, predicted fertilizer is 19\nOutput Text: Hey Nam, thanks for using SimpliFarm! Our analysis reports that your estimated two week total was $123.1, with your water per square foot coming out to $19, and your fertilizer per square foot at $48. Our analysis shows that your predicted plant stress is at 38%, predicted water usage at $39, and predicted fertilizer usage at $19. \n--\nSample: '
